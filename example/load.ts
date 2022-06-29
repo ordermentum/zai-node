f5aafbc7b342021abf3fac0ee3b3e03acc383458
John D'Agostino <john@ordermentum.com>
2022-06-29 12:36:28 +1000

    add ts-node and another example

---
+++ example/load.ts
@@ -0,0 +1,26 @@
+import { createClient } from '../src';
+
+const CLIENT_ID = '';
+const CLIENT_SECRET = '';
+const CLIENT_SCOPE = '';
+
+async function runner() {
+  const client = createClient({
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    scope: CLIENT_SCOPE,
+    baseURL: 'https://au-0000.api.assemblypay.com/',
+  });
+
+  const data = await client.transactions.showSupplementaryData('');
+  const user = await client.users.showUser('');
+
+  console.log(data, user);
+}
+
+runner()
+  .then(() => process.exit())
+  .catch(e => {
+    console.error(e);
+    process.exit();
+  });
